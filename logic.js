a45b26fcd12f64f8b93660b12b59531dc41b607f
Lafuente <0273827@students.dadeschools.net>
2018-04-12 14:12:23 -0400

    First day of trivia game

---
+++ logic.js
@@ -0,0 +1,23 @@
+var panel = $("");
+var countStartNumber = 30;
+
+// question set
+var questions = [
+{
+	question: "What does "GTA" stand for?",
+	answers: ["Grand Theft Auto", "Gernades Tanks and Airplanes" , "Guns Training and Ammo"]
+	correctAnswer: "Grand Theft Auto"
+	image: "assets/GTA.jpg"
+},
+
+{
+	question: "What was the first video game?",
+	answers: ["Pong", "Tetris" , "Pac man"]
+	correctAnswer: "Pong"
+	image: "asssets/Pong.jpg"
+}
+
+
+
+
+]
